d276a6f9396b070b62af4d6cd71357057211526b
Shrihari Sambath <shrihari.sambath@gmail.com>
2024-01-21 23:47:52 +0530

    Go Middlewares - 01212024

--- Gin_GO/server.go
+++ Gin_GO/server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	gitdump "github.com/tpkeeper/gin-dump"
 	"mygo.com/hari/golang-gin-poc/controller"
+	"mygo.com/hari/golang-gin-poc/middlewares"
 	"mygo.com/hari/golang-gin-poc/service"
 )
 
@@ -11,8 +16,17 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutput()
+	server := gin.New()
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gitdump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
